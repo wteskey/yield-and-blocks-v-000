4c324bcca50fe96d701fdb042e7a11ea45094355
William J Teskey <willjteskey@gmail.com>
2018-06-11 19:48:43 +0000

    automatically backed up by learn

---
+++ lib/hello.rb
@@ -0,0 +1,12 @@
+def hello_t(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+  array
+end
